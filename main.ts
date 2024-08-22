aba2aaeadd8fbb630594e752b0f8420963dbd132
PercyPray <ppray2028@communityschoolnaples.org>
2024-08-22 12:17:42 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,34 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-    enemy4.setPosition(randint(0, 10), randint(0, 10))
+namespace SpriteKind {
+    export const boss = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.boss, function (sprite, otherSprite) {
+    info.changeLifeBy(-5)
+    enemy5.setPosition(randint(0, 160), randint(0, 120))
+})
+function collectable () {
+    Collectable = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . e e . . . . . 
+        . . . . . . . . e e e . . . . . 
+        . . . . . . . e e e . . . . . . 
+        . . . . . . . e e . . . . . . . 
+        . . . . . . e e e e . . . . . . 
+        . . . . . e e f e e e . . . . . 
+        . . . . e e f f f e e e . . . . 
+        . . . e e e f f f e e e e . . . 
+        . . e e e e e f f e e e e e . . 
+        . . . e e e f f f e e e e . . . 
+        . . . . e e f f e e e e . . . . 
+        . . . . . e f f f e e . . . . . 
+        . . . . . . e f e e . . . . . . 
+        . . . . . . e e e e . . . . . . 
+        . . . . . . . e e . . . . . . . 
+        `, SpriteKind.Food)
+    Collectable.setPosition(randint(0, 160), randint(0, 120))
+}
+info.onCountdownEnd(function () {
+    game.setGameOverMessage(true, "GAME OVER!")
+    game.gameOver(true)
 })
 function spanwn_enemy () {
     enemy4 = sprites.create(img`
@@ -20,11 +48,16 @@ function spanwn_enemy () {
         . . . . 1 f 1 f f f f . . . . . 
         . . . . 1 f f f f f . . . . . . 
         . . . . 1 1 1 1 f f . . . . . . 
-        `, SpriteKind.Player)
+        `, SpriteKind.Enemy)
     enemy4.setPosition(66, 61)
     enemy4.setVelocity(100, 50)
     enemy4.setBounceOnWall(true)
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite)
+    collectable()
+})
 function spawn_Get_upppp () {
     Get_upppp = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -49,7 +82,6 @@ function spawn_Get_upppp () {
     Get_upppp.setPosition(-3, 117)
 }
 function mikeistrash () {
-    let mySprite: Sprite = null
     enemy5 = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . 1 1 1 1 1 1 . . . . . 
@@ -67,16 +99,24 @@ function mikeistrash () {
         . . . . . . 4 4 4 4 . . . . . . 
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
-    enemy4.setPosition(66, 61)
-    mySprite.setVelocity(100, 90)
-    enemy4.setBounceOnWall(true)
+        `, SpriteKind.boss)
+    enemy5.setPosition(66, 61)
+    enemy5.setVelocity(50, 50)
+    enemy5.setBounceOnWall(true)
 }
-let enemy5: Sprite = null
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.setPosition(randint(0, 160), randint(0, 120))
+})
 let Get_upppp: Sprite = null
 let enemy4: Sprite = null
+let Collectable: Sprite = null
+let enemy5: Sprite = null
+collectable()
 spanwn_enemy()
 mikeistrash()
 spawn_Get_upppp()
 scene.setBackgroundColor(1)
 info.setLife(5)
+info.setScore(0)
+game.setGameOverScoringType(game.ScoringType.HighScore)
